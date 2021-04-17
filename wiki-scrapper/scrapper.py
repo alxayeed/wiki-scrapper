f8ba788112d3857009bdad8817696583e77cad42
alxayeed <alxayeed@gmail.com>
2021-04-17 19:54:43 +0600

    single row data is collected

--- wiki-scrapper/scrapper.py
+++ wiki-scrapper/scrapper.py
@@ -4,10 +4,31 @@ import requests
 
 app = Flask(__name__)
 
+# configure bs4
+url = 'https://en.m.wikipedia.org/wiki/List_of_largest_Internet_companies'
+response = requests.get(url).text
+bs_object = BeautifulSoup(response, 'html.parser')
+
+data = bs_object.find('table', class_='wikitable sortable mw-collapsible')
+caption = data.caption.text
+all_rows = data.find_all('tr')
+
+
+# this list will contain each row of the table as inner list
+table_data = []
+for tr in all_rows[1:6]:
+    row = []
+    for t in tr.select('td')[:-1]:
+        row.extend([t.text.strip()])
+    table_data.append(row)
+    break
+
+print(table_data)
+
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('index.html', caption=caption, table_data=table_data)
 
 
 if __name__ == "__main__":
